3194300785d4db23fa94b2564d6d66b0fd45756c
Agata D. <124964478+Agata10@users.noreply.github.com>
2024-07-17 17:17:43 +0000

    Add challenge 7

--- 01_restaurant/challenges/01_07_attendevent_ch.sql
+++ 01_restaurant/challenges/01_07_attendevent_ch.sql
@@ -2,3 +2,15 @@
 
 -- The customer 'atapley2j@kinetecoinc.com' will be in
 -- attendance, and will bring 3 friends.
+
+INSERT INTO
+AnniversaryAttendends(CustomerId, PartySize)
+VALUES(
+(SELECT
+CustomerId
+FROM
+Customers
+WHERE
+Email = 'atapley2j@kinetecoinc.com'), 3)
+
+SELECT * from AnniversaryAttendends;
