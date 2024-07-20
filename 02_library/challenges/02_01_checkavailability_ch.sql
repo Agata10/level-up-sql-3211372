3de921f7051f6a220e6d90d08ab2e95630b7246e
Agata D. <124964478+Agata10@users.noreply.github.com>
2024-07-20 20:51:59 +0000

    Add part 2, challenge 1

--- 02_library/challenges/02_01_checkavailability_ch.sql
+++ 02_library/challenges/02_01_checkavailability_ch.sql
@@ -1,2 +1,38 @@
 -- Determine how many copies of the book 'Dracula'
 -- are available for library patrons to borrow.
+
+SELECT
+count(b.BookId) as TotalBooks
+FROM
+Books as b
+WHERE
+title = 'Dracula';
+-- 12
+-- 60
+-- 73
+
+SELECT
+count(l.BookId) as CheckedOut
+FROM
+Loans as l
+join books as b
+on b.BookId = l.BookId
+WHERE
+returnedDate is null AND (l.BookId in (12,60,73))
+
+SELECT
+(SELECT
+count(b.BookId) as TotalBooks
+FROM
+Books as b
+WHERE
+title = 'Dracula') -
+(SELECT
+count(l.BookId) as CheckedOut
+FROM
+Loans as l
+join books as b
+on b.BookId = l.BookId
+WHERE
+returnedDate is null AND (l.BookId in (12,60,73)))
+AS BooksAvaiable;
