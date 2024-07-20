e784a72992ae77002d33632abde276af8380d9a5
Agata D. <124964478+Agata10@users.noreply.github.com>
2024-07-20 21:11:27 +0000

    Add part 2, challenge 4

--- 02_library/challenges/02_04_booksdue_ch.sql
+++ 02_library/challenges/02_04_booksdue_ch.sql
@@ -2,3 +2,18 @@
 -- to the library on July 13, 2022.
 -- Provide the due date, the book title, and
 -- the borrower's first name and email address.
+
+SELECT
+b.title,
+l.dueDate,
+p.FirstName,
+p.LastName,
+p.email 
+FROM
+loans as l
+join
+Patrons as p
+on p.PatronId = l.PatronId
+join Books as b
+on b.BookId = l.BookId
+WHERE l.dueDate = '2022-07-13' and l.returnedDate is null
