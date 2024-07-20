1146717a58a376f91207b9789586bb6d2632b7b2
Agata D. <124964478+Agata10@users.noreply.github.com>
2024-07-20 21:46:24 +0000

    Add part 2, challenge 7

--- 02_library/challenges/02_07_featurebooks_ch.sql
+++ 02_library/challenges/02_07_featurebooks_ch.sql
@@ -2,3 +2,13 @@
 
 -- Make a pick list of books published from 1890-1899 
 -- which are not currently checked out.
+
+SELECT
+*
+FROM
+Books as b
+WHERE
+b.published >= 1890 AND b.published <= 1899 
+AND (BookId not in (SELECT BookId from loans where returneddate is null)
+)
+order by Title
