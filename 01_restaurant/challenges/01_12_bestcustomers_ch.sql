1b1ab28608c598fe1c0617852c64c493cad56ffa
Agata D. <124964478+Agata10@users.noreply.github.com>
2024-07-20 18:09:11 +0000

    Add challenge 12

--- 01_restaurant/challenges/01_12_bestcustomers_ch.sql
+++ 01_restaurant/challenges/01_12_bestcustomers_ch.sql
@@ -1,3 +1,17 @@
 -- Identify a few customers who have ordered delivery
 -- from the restaurant the most often, so we can send
 -- them a promotional coupon.
+
+SELECT
+c.firstname, 
+c.lastname,
+o.customerid,
+count(o.orderid) AS OrdersTotal
+FROM
+Orders as o
+inner join Customers as c
+on c.customerid = o.customerid
+group by o.customerid
+order by OrdersTotal DESC
+limit 10
+
