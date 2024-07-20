ac85e1185b73d160b570744f075c102bdca9d20c
Agata D. <124964478+Agata10@users.noreply.github.com>
2024-07-20 16:42:41 +0000

    Add challenge 8

--- 01_restaurant/challenges/01_08_findreservation_ch.sql
+++ 01_restaurant/challenges/01_08_findreservation_ch.sql
@@ -5,3 +5,15 @@
 -- Stevensen, Stephensen, Stevenson, Stephenson, Stuyvesant
 
 -- There are four people in the party. Today is June 14th.
+SELECT
+r.CustomerId, c.FirstName, c.LastName, r.PartySize, r.Date, r.ReservationId
+FROM
+Reservations as r
+Inner Join Customers as c
+ON
+c.CustomerId = r.CustomerId
+WHERE
+LastName like 'Ste%' 
+AND PartySize = 4 
+order by
+r.date desc
