6009d9164336a6a809dd79e417086f9d5ef10c2c
Agata D. <124964478+Agata10@users.noreply.github.com>
2024-07-20 17:46:18 +0000

    Add challenge 11

--- 01_restaurant/challenges/01_11_trackfavorites_ch.sql
+++ 01_restaurant/challenges/01_11_trackfavorites_ch.sql
@@ -2,3 +2,27 @@
 
 -- Set Cleo Goldwater's favorite dish to
 -- the Quinoa Salmon Salad.
+
+Update 
+Customers
+Set
+FavoriteDish = (SELECT
+DishID
+FROM
+Dishes
+WHERE
+Name = 'Quinoa Salmon Salad')
+WHERE
+FirstName = 'Cleo' AND LastName = 'Goldwater';
+
+SELECT 
+c.FirstName, c.LastName, c.FavoriteDish, d.Name
+FROM Customers as c
+join dishes as d
+on d.DishID = c.FavoriteDish
+WHERE c.FirstName = 'Cleo' and c.LastName='Goldwater';
+
+SELECT
+*
+FROM Customers as c
+WHERE c.FirstName = 'Cleo' and c.LastName='Goldwater';
