bf6984a9e462f4e2e42692d2c7f313c3264bef01
Agata D. <124964478+Agata10@users.noreply.github.com>
2024-07-20 22:06:46 +0000

    Add part 2, challenge 8

--- 02_library/challenges/02_08_bookstats_ch.sql
+++ 02_library/challenges/02_08_bookstats_ch.sql
@@ -2,6 +2,29 @@
 
 -- Report 1: Show how many titles were published 
 -- in each year.
+SELECT
+published,
+count(Distinct(title)) as count
+from
+books 
+group by 
+published
+order by count desc
+
 
 -- Report 2: Show the five books that have been
 -- checked out the most.
+SELECT
+b.title,
+b.Barcode,
+b.author,
+l.bookid, 
+count(l.loanDate) as TotalCheckout
+FROM
+loans as l
+join books as b
+on l.bookid = b.bookid
+group by 
+b.title
+order by TotalCheckout desc
+limit 5
