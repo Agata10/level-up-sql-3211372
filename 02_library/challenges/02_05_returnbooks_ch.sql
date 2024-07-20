158426d31e6910886fe58a8590af6a1b9908f6f2
Agata D. <124964478+Agata10@users.noreply.github.com>
2024-07-20 21:17:35 +0000

    Add part 2, challenge 5

--- 02_library/challenges/02_05_returnbooks_ch.sql
+++ 02_library/challenges/02_05_returnbooks_ch.sql
@@ -4,3 +4,43 @@
 -- Book 1: 6435968624
 -- Book 2: 5677520613
 -- Book 3: 8730298424
+
+UPDATE
+Loans
+SET
+ReturnedDate = '2022-07-05'
+WHERE
+(BookID = (
+SELECT
+BookId
+FROM
+Books
+WHERE
+Barcode = 6435968624) AND
+ReturnedDate is null) OR
+(BookId = (
+SELECT
+BookId
+FROM
+Books
+WHERE
+Barcode = 5677520613)  AND
+ReturnedDate is null)  OR
+
+(BookId = (
+SELECT
+BookId
+FROM
+Books
+WHERE
+Barcode = 8730298424)
+ AND
+ReturnedDate is null) 
+
+
+SELECT
+*
+from
+Loans
+WHERE
+ReturnedDate =
