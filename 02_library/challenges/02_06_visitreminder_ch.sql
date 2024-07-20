3c7e705792c3e7747941297198f33527d451b250
Agata D. <124964478+Agata10@users.noreply.github.com>
2024-07-20 21:36:48 +0000

    Add part 2, challenge 6

--- 02_library/challenges/02_06_visitreminder_ch.sql
+++ 02_library/challenges/02_06_visitreminder_ch.sql
@@ -1,2 +1,16 @@
 -- Prepare a report of the library patrons
 -- who have checked out the fewest books.
+SELECT
+p.firstName,
+p.email,
+p.lastName,
+l.patronID,
+count(l.patronID) as CheckedBooks
+FROM
+loans as l
+join patrons as p
+on p.patronID = l.patronID
+GROUP BY
+l.patronid
+order by CheckedBooks
+limit 10
