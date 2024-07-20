2a8e679a1414c13f35f567e9ee1190898cffa12f
Agata D. <124964478+Agata10@users.noreply.github.com>
2024-07-20 17:37:58 +0000

    Add challenge 10

--- 01_restaurant/challenges/01_10_takeorder_ch.sql
+++ 01_restaurant/challenges/01_10_takeorder_ch.sql
@@ -7,3 +7,70 @@
 -- 1 Tropical Blue Smoothie
 -- Delivery date and time: September 20, 2022 @ 2PM (14:00)
 -- There are no taxes or other fees.
+
+SELECT
+* 
+FROM
+Customers
+WHERE
+FirstName = 'Loretta' AND LastName = 'Hundey' AND address= '6939 Elka Place';
+-- 70
+
+SELECT
+DishId, Name, Price
+FROM Dishes
+WHERE
+Name in ('House Salad','Mini Cheeseburgers','Tropical Blue Smoothie');
+
+INSERT into
+ORDERS(CustomerID, OrderDate)
+VALUES(
+(SELECT 
+CustomerID
+FROM
+Customers
+WHERE
+FirstName = 'Loretta' AND LastName = 'Hundey' AND address= '6939 Elka Place'), '2022-09-20 14:00:00');
+
+SELECT 
+* 
+FROM 
+ORDERS
+WHERE 
+CustomerID = (SELECT 
+CustomerID
+FROM
+Customers
+WHERE
+FirstName = 'Loretta' AND LastName = 'Hundey' AND address= '6939 Elka Place') AND
+OrderDate = '2022-09-20 14:00:00'
+-- 1001
+
+INSERT into
+OrdersDishes(OrderId,DishId)
+VALUES
+(1001,(SELECT
+DishId
+FROM Dishes
+WHERE
+Name = 'House Salad')),
+(1001,(SELECT
+DishId
+FROM Dishes
+WHERE
+Name = 'Mini Cheeseburgers')),
+(1001,(SELECT
+DishId
+FROM Dishes
+WHERE
+Name = 'Tropical Blue Smoothie'));
+
+
+SELECT
+*
+FROM
+OrdersDishes
+order by
+OrderId desc
+
+
