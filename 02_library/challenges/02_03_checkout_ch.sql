907a3e801831c651fa41bdde286d5a7d72d6a954
Agata D. <124964478+Agata10@users.noreply.github.com>
2024-07-20 21:04:37 +0000

    Add part 2, challenge 3

--- 02_library/challenges/02_03_checkout_ch.sql
+++ 02_library/challenges/02_03_checkout_ch.sql
@@ -3,3 +3,36 @@
 -- Book 2: Great Expectations, 4043822646
 -- The checkout date is August 25, 2022 and the 
 -- due date is September 8, 2022.
+
+SELECT
+BookId
+FROM
+Books
+WHERE barcode = 2855934983;
+
+SELECT
+*
+from
+Patrons
+WHERE email = 'jvaan@wisdompets.com'
+-- 50
+
+INSERT INTO
+Loans(BookId,PatronId,LoanDate,DueDate)
+VALUES
+((SELECT
+BookId
+FROM
+Books
+WHERE barcode = 2855934983), 50, '2022-08-25','2022-09-08'),
+((SELECT
+BookId
+FROM
+Books
+WHERE barcode = 4043822646), 50, '2022-08-25','2022-09-08')
+
+SELECT
+*
+from
+Loans
+WHERE LoanDate='2022-08-25'
