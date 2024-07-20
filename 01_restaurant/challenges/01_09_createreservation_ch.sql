e60b583949753022bcc90d6150da5eb3e83c9c6d
Agata D. <124964478+Agata10@users.noreply.github.com>
2024-07-20 16:54:00 +0000

    Add challenge 9

--- 01_restaurant/challenges/01_09_createreservation_ch.sql
+++ 01_restaurant/challenges/01_09_createreservation_ch.sql
@@ -4,3 +4,31 @@
 -- Use the following information:
 -- Sam McAdams (smac@kinetecoinc.com), for 5 people
 -- on August 12, 2022 at 6PM (18:00)
+
+
+Select
+*
+FROM
+Customers 
+WHERE
+FirstName = 'Sam' AND LastName='McAdams' AND email = 'smac@kinetecoinc.com'
+
+INSERT into
+Customers(FirstName,LastName,email)
+VALUES ('Sam','McAdams','smac@kinetecoinc.com')
+
+INSERT into 
+reservations(CustomerId,Date,PartySize)
+VALUES ((Select
+CustomerId
+FROM
+Customers 
+WHERE
+email = 'smac@kinetecoinc.com'),'2022-08-12 18:00:00', 5)
+
+Select
+*
+FROM reservations
+WHERE 
+date = '2022-08-12 18:00:00'
+
